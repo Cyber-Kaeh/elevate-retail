5c359eee8f98c6bf36ff329b47a4c5000c314653
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-03-17 17:56:21 +0000

    Set up initial project structure with Flask, Jinja templates, and tested Codespaces.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Elevate Retail!"
+    return render_template('landing.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/inventory')
+def inventory():
+    return render_template('inventory.html')
+
+@app.route('/purchasing')
+def purchasing():
+    return render_template('purchasing.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-  """Set debug=False for production!"""
+"""Set debug=False and run behind a WSGI server for production!"""
