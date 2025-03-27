713d5af2eecb85bd916644bc11ea3ea467ecd2ba
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-03-27 20:51:38 +0000

    Shopping cart is cobbled together. Needs serious work on the front end though

--- src/routes/cart_routes.py
+++ src/routes/cart_routes.py
@@ -1,30 +1,24 @@
-from flask import Blueprint, session, redirect, url_for, request
-from src.controllers.session_controller import create_session, get_session_data, update_session, delete_session
+from flask import Blueprint, session, redirect, url_for, request, render_template
+from src.controllers.inventory_controller import get_inventory_item_by_id
 
 cart_bp = Blueprint('cart', __name__)
 
 
 @cart_bp.route('/add_to_cart/<int:item_id>', methods=['GET'])
 def add_to_cart(item_id):
-    cart = session.get('cart', [])
-    cart.append(item_id)
-    session['cart'] = cart
-    # Store the session in the database
-    update_session(session.sid, str(session))
+    if 'cart' not in session:
+        session['cart'] = []
+    session['cart'].append(item_id)
     return redirect(url_for('inventory.view_inventory'))
 
 
 @cart_bp.route('/remove_from_cart/<int:item_id>', methods=['GET'])
 def remove_from_cart(item_id):
-    cart = session.get('cart', [])
-    cart.remove(item_id)
-    session['cart'] = cart
-    # Store the session in the database
-    update_session(session.sid, str(session))
+    if 'cart' in session:
+        session['cart'].remove(item_id)
     return redirect(url_for('inventory.view_inventory'))
 
 
 @cart_bp.route('/cart', methods=['GET'])
 def view_cart():
-    cart = session.get('cart', [])
-    return render_template('cart.html', cart=cart)
+    return render_template('cart.html', cart=session.get('cart', []))
